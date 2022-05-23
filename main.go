3aa3e867bde4d1d2f8ad2fe448f0ca953826da08
hilman19 <hilmanfitriana19@gmail.com>
2022-05-23 15:06:12 +0700

    upload file to dropbox

--- main.go
+++ main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-	"time"
+	"net/http"			
+	"encoding/json"
+	"mime/multipart"
 )
 
-const MAX_UPLOAD_SIZE = 1024 * 1024 * 100 // 100MB
+const MAX_UPLOAD_SIZE = 1024 * 1024 * 100 // 100MB, note : this code only work to upload file below 150MB
+
+const DROPBOX_DEFAULT_PATH = "/file-upload"		// path to save the file in dropbox -> Path will be : Dropbox/Application/project_name/file-upload
+
+const DROPBOX_ACCESS_TOKEN = "<get access token>"
+
+const DROPBOX_UPLOAD_URL = "https://content.dropboxapi.com/2/files/upload"
+
+const DROPBOX_GET_FILE_URL = "https://api.dropboxapi.com/2/sharing/create_shared_link_with_settings"
 
 // Progress is used to track the progress of a file upload.
 // It implements the io.Writer interface so it can be passed
@@ -19,6 +26,16 @@ type Progress struct {
 	TotalSize int64
 	BytesRead int64
 }
+
+// Config is used to set the configuration in order to upload to dropbox
+type Config struct {	
+	Autorename     bool   `json:"autorename"`
+	Mode           string `json:"mode"`
+	Mute           bool   `json:"mute"`
+	Path           string `json:"path"`
+	StrictConflict bool   `json:"strict_conflict"`
+}
+
 
 // Write is used to satisfy the io.Writer interface.
 // Instead of writing somewhere, it simply aggregates
@@ -64,8 +81,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		if fileHeader.Size > MAX_UPLOAD_SIZE {
 			http.Error(w, fmt.Sprintf("The uploaded image is too big: %s. Please use an image less than 100MB in size", fileHeader.Filename), http.StatusBadRequest)
 			return
-		}
-
+		}		
 		file, err := fileHeader.Open()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -92,33 +108,34 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
-		err = os.MkdirAll("./uploads", os.ModePerm)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		f, err := os.Create(fmt.Sprintf("./uploads/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		defer f.Close()
-
-		pr := &Progress{
-			TotalSize: fileHeader.Size,
-		}
-
-		_, err = io.Copy(f, io.TeeReader(file, pr))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
+		fmt.Println("Upload to Dropbox")
+		uploadDropbox(file,fileHeader.Filename)
+		
 	}
 
 	fmt.Fprintf(w, "Upload successful")
+}
+
+// upload file to dropbox using http request
+func uploadDropbox(file multipart.File, fileName string){
+	req, err := http.NewRequest("POST", DROPBOX_UPLOAD_URL, file)
+	if err != nil {
+		panic(err)		
+	}	
+	config := Config{false, "add", false, fmt.Sprintf("%s/%s", DROPBOX_DEFAULT_PATH, fileName), false}
+	jsonConfig, err := json.Marshal(config)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s",DROPBOX_ACCESS_TOKEN))
+	req.Header.Set("Dropbox-Api-Arg", string(jsonConfig))
+	req.Header.Set("Content-Type", "application/octet-stream")	
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}	
+	defer resp.Body.Close()
 }
 
 func main() {
