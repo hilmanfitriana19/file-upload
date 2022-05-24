844af98835f6427a2930d0aed0f64b68800e5139
hilman19 <hilmanfitriana19@gmail.com>
2022-05-24 10:08:47 +0700

    add get url of uploaded file

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
-	"net/http"			
-	"encoding/json"
 	"mime/multipart"
+	"net/http"
 )
 
-const MAX_UPLOAD_SIZE = 1024 * 1024 * 100 // 100MB, note : this code only work to upload file below 150MB
+const MAX_UPLOAD_SIZE = 1024 * 1024 * 100 // 100MB, note : this code only work to upload file below
 
-const DROPBOX_DEFAULT_PATH = "/file-upload"		// path to save the file in dropbox -> Path will be : Dropbox/Application/project_name/file-upload
+const DROPBOX_DEFAULT_PATH = "/file-upload" // path to save the file in dropbox -> Path will be : Dropbox/Application/project_name/file-upload
 
 const DROPBOX_ACCESS_TOKEN = "<get access token>"
 
@@ -28,7 +30,7 @@ type Progress struct {
 }
 
 // Config is used to set the configuration in order to upload to dropbox
-type Config struct {	
+type Config struct {
 	Autorename     bool   `json:"autorename"`
 	Mode           string `json:"mode"`
 	Mute           bool   `json:"mute"`
@@ -36,6 +38,18 @@ type Config struct {
 	StrictConflict bool   `json:"strict_conflict"`
 }
 
+// Payload Configuration for request url of uploaded file
+type Settings struct {
+	Access              string `json:"access"`
+	AllowDownload       bool   `json:"allow_download"`
+	Audience            string `json:"audience"`
+	RequestedVisibility string `json:"requested_visibility"`
+}
+
+type Payload struct {
+	Path     string   `json:"path"`
+	Settings Settings `json:"settings"`
+}
 
 // Write is used to satisfy the io.Writer interface.
 // Instead of writing somewhere, it simply aggregates
@@ -81,7 +95,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		if fileHeader.Size > MAX_UPLOAD_SIZE {
 			http.Error(w, fmt.Sprintf("The uploaded image is too big: %s. Please use an image less than 100MB in size", fileHeader.Filename), http.StatusBadRequest)
 			return
-		}		
+		}
 		file, err := fileHeader.Open()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -108,34 +122,81 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		fmt.Println("Upload to Dropbox")
-		uploadDropbox(file,fileHeader.Filename)
-		
+		uploadDropbox(file, fileHeader.Filename)
+
+		url := getUploadedFileDropbox(fileHeader.Filename)
+
+		fmt.Println(url)
+
 	}
 
 	fmt.Fprintf(w, "Upload successful")
 }
 
 // upload file to dropbox using http request
-func uploadDropbox(file multipart.File, fileName string){
+func uploadDropbox(file multipart.File, fileName string) {
 	req, err := http.NewRequest("POST", DROPBOX_UPLOAD_URL, file)
 	if err != nil {
-		panic(err)		
-	}	
+		panic(err)
+	}
 	config := Config{false, "add", false, fmt.Sprintf("%s/%s", DROPBOX_DEFAULT_PATH, fileName), false}
 	jsonConfig, err := json.Marshal(config)
 	if err != nil {
 		panic(err)
 	}
 
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s",DROPBOX_ACCESS_TOKEN))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", DROPBOX_ACCESS_TOKEN))
 	req.Header.Set("Dropbox-Api-Arg", string(jsonConfig))
-	req.Header.Set("Content-Type", "application/octet-stream")	
+	req.Header.Set("Content-Type", "application/octet-stream")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		panic(err)
-	}	
+	}
 	defer resp.Body.Close()
+}
+
+// get url for uploaded file, return the dropbox url
+func getUploadedFileDropbox(fileName string) string {
+
+	data := Payload{
+		fmt.Sprintf("%s/%s", DROPBOX_DEFAULT_PATH, fileName),
+		Settings{
+			"viewer", true, "public", "public",
+		},
+	}
+	// convert to json
+	payloadBytes, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	// conver to byte
+	body := bytes.NewReader(payloadBytes)
+
+	req, err := http.NewRequest("POST", DROPBOX_GET_FILE_URL, body)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", DROPBOX_ACCESS_TOKEN))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+	// read response body
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("client: could not read response body: %s\n", err)
+		panic(err)
+	}
+
+	var result map[string]interface{} //template to convert response body to map
+	json.Unmarshal(resBody, &result)  //convert to json
+
+	url_data := result["url"].(string) //get url
+
+	return url_data
 }
 
 func main() {
